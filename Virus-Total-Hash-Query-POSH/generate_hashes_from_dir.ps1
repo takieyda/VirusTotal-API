dddb77cd3a3e954c32ae510d2594be87f27a5a65
takieyda <takieyda@gmail.com>
2023-10-18 00:42:37 -0400

    updates

--- Virus-Total-Hash-Query-POSH/generate_hashes_from_dir.ps1
+++ Virus-Total-Hash-Query-POSH/generate_hashes_from_dir.ps1
@@ -7,25 +7,33 @@ function get_dir {
     Split-Path $invoke.MyCommand.Path
 }
 
-$script_path = get_dir
-$num_files = (Get-ChildItem -Recurse | Where-Object { -not $_.PSIsContainer }).Count - 1
+If (-not $Args[0]) {
+    $Script:script_path = get_dir
+    $Script:num_files = (Get-ChildItem -Path $script_path -Recurse | Where-Object { -not $_.PSIsContainer }).Count - 1
+} Else {
+    $Script:cur_dir = pwd
+    $Script:script_path = $Args[0]
+    $Script:num_files = (Get-ChildItem -Path $script_path -Recurse | Where-Object { -not $_.PSIsContainer }).Count
+}
 
-$hashes = [ordered]@{}
+
+$Script:hashes = [ordered]@{}
 
 function pause {
     Read-Host "`n`nPress Enter to continue." | Out-Null
+    If ($cur_dir) { Set-Location -Path $Script:cur_dir }
 }
 
 function welcome {
     Write-Host "`n`n================================`n|      Generating Hashes       |`n================================"
-    Write-Host "`nPath:  " -ForegroundColor Yellow -NoNewLine; Write-Host $script_path
-    Write-Host "Files: " -ForegroundColor Yellow -NoNewLine; Write-Host $num_files"`n"
+    Write-Host "`nPath:  " -ForegroundColor Yellow -NoNewLine; Write-Host $Script:script_path
+    Write-Host "Files: " -ForegroundColor Yellow -NoNewLine; Write-Host $Script:num_files"`n"
     Write-Host "================================`n"
 }
 
 function generate_hashes {
     $cnt = 0
-    (Get-ChildItem -Recurse $script_path) | ForEach-Object {
+    (Get-ChildItem -Path $script_path -Recurse) | ForEach-Object {
         If (($_.Name -ne "generate_hashes_from_dir.ps1") -and (-not $_.PSIsContainer)) {
             $cnt += 1
             Set-Location $script_path
@@ -62,7 +70,7 @@ function save_file {
         Write-Host "`n*** Nothing to export ***" -ForegroundColor Yellow
     }
     
-    Write-Host "`n`n--------------------------------"    
+    Write-Host "`n`n--------------------------------"
 }
 
 welcome
